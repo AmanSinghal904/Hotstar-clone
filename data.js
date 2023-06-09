f0ad72687fa0ae03e18029ccec54c627ed6db4f8
aman singhal <Aman Singhal>
2023-06-09 11:07:54 +0530

    commit

---
+++ data.js
@@ -0,0 +1,33 @@
+let movies = [
+    {
+      name: "Falcon and the Winter Soldier",
+      des:
+        "Following the events of 'Avengers:Endgame', Sam Wilson and Bucky Barnes team up in a global adventurethat tests their abilities and their patience.",
+      image: "images/slider 2.png"
+    },
+    {
+      name: "Loki",
+      des:
+        "The mercurial villain Loki resumes his role as the God of Mischief in a new series that takes place after the events of Avengers:Endgame.",
+      image: "images/slider 1.png"
+    },
+    {
+      name: "WandaVision",
+      des:
+        "Wanda Maximoff and Vision-two superpowered beings living idealised suburban lives-begin to suspect that everything is not as it seems.",
+      image: "images/slider 3.png"
+    },
+    {
+      name: "Raya and the Last Dragon",
+      des:
+        "Raya, a fallen princess, must track down the legendary last dragon to stop the evil forces that have returned and threaten her world.",
+      image: "images/slider 4.png"
+    },
+    {
+      name: "Luca",
+      des:
+        "The movie is a coming-of-age story about one young boy experiencing an unforgettable summer filled with gellato, pasta and endless scooter rides.",
+      image: "images/slider 5.png"
+    }
+  ];
+  
